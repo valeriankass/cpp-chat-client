aee7b574ad52bda997a6db834d4c5db4b2dd60ce
valeriankass <46694577+valeriankass@users.noreply.github.com>
2021-04-22 16:25:32 +0200

    Add files via upload

---
+++ CMakeFiles/cpp-chat-client.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vu-student/Desktop/CN2019/cpp-chat-client/CircularLineBuffer.cpp" "/home/vu-student/Desktop/CN2019/cpp-chat-client/CMakeFiles/cpp-chat-client.dir/CircularLineBuffer.cpp.o"
+  "/home/vu-student/Desktop/CN2019/cpp-chat-client/Client.cpp" "/home/vu-student/Desktop/CN2019/cpp-chat-client/CMakeFiles/cpp-chat-client.dir/Client.cpp.o"
+  "/home/vu-student/Desktop/CN2019/cpp-chat-client/main.cpp" "/home/vu-student/Desktop/CN2019/cpp-chat-client/CMakeFiles/cpp-chat-client.dir/main.cpp.o"
+  "/home/vu-student/Desktop/CN2019/cpp-chat-client/vusocket.cpp" "/home/vu-student/Desktop/CN2019/cpp-chat-client/CMakeFiles/cpp-chat-client.dir/vusocket.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/cpp-chat-client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-chat-client.dir/main.cpp.o"
+  "CMakeFiles/cpp-chat-client.dir/Client.cpp.o"
+  "CMakeFiles/cpp-chat-client.dir/vusocket.cpp.o"
+  "CMakeFiles/cpp-chat-client.dir/CircularLineBuffer.cpp.o"
+  "cpp-chat-client.pdb"
+  "cpp-chat-client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-chat-client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
